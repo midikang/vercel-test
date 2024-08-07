f9b14afebcfc7240d425e162df3034d4ac6ef10f
凌览 <lihai_js@163.com>
2024-08-07 16:42:52 +0800

    build: 创建项目

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { Router } = require('express');
+const app = express();
+const router = Router();
+
+
+router.get('/hello-vercel', async (req, res) => {
+    res.status(200).json({ code: 200, data: 'hello-vercel', msg: '请求成功' })
+})
+
+
+app.use('/', router)
+app.listen(9000, () => console.log("Server ready on port 9000"));
+
+module.exports = app;
