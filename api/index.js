d09c7eaf3b547ff268a9590b458d5bf3cb9bec50
Midi <midi13@gmail.com>
2024-08-10 00:00:51 +0800

    Update index.js
    add hello

--- api/index.js
+++ api/index.js
@@ -8,6 +8,10 @@ router.get('/hello-vercel', async (req, res) => {
     res.status(200).json({ code: 200, data: 'hello-vercel', msg: '请求成功' })
 })
 
+router.get('/hello', async (req, res) => {
+    res.status(200).json({ code: 200, data: '晚上好', msg: '请求成功' })
+})
+
 
 app.use('/', router)
 app.listen(9000, () => console.log("Server ready on port 9000"));
